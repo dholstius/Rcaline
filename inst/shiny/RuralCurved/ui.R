cb87507f39cd69eaeaf5abbb63c769216652dfa3
David Holstius <david.holstius@gmail.com>
2013-03-04 14:42:36 -0800

    restrict windSpeed input to 1.0 or greater

--- inst/shiny/RuralCurved/ui.R
+++ inst/shiny/RuralCurved/ui.R
@@ -10,7 +10,7 @@ shinyUI(pageWithSidebar(
     sliderInput("windBearing", "Wind bearing (degrees):", 
                 min = 0, max = 359, value = 45, step = 15),
     sliderInput("windSpeed", "Wind speed (m/s):", 
-                min = 0, max = 7, value = 2, step = 0.1),
+                min = 1, max = 7, value = 2, step = 0.1),
     sliderInput("stabilityClass", "Stability class:", 
                 min = 1, max = 6, value = 6, step = 1),
     sliderInput("mixingHeight", "Mixing height (m):", 

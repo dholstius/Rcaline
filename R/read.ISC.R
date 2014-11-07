c63d8d65e41e9aea86145a7a1562aed7ac8cdb01
David Holstius <david.holstius@gmail.com>
2014-11-07 15:20:44 -0800

    deprecate read.ISC in favor of ISCFile

--- R/read.ISC.R
+++ R/read.ISC.R
@@ -1,7 +1,8 @@
+#' @export
 read.ISC <- function(
 	file
 ) {
-	warning("read.ISC() is deprecated. See ?Meteorology or ?ISCFile for more.")
+  .Deprecated(ISCFile, msg="read.ISC() is deprecated. Please use ISCFile() or Meteorology() instead.")
 	header <- readLines(file, 1)
 	header.tokens <- strsplit(header, '\\s+')[[1]]
 	result <- lapply(as.list(header.tokens)[-1], as.integer)
@@ -11,26 +12,26 @@ read.ISC <- function(
 		c("4I2","2F9","F6","I2","2F7"),
 		skip = 1,
 		col.names = c(
-			"year", 
-			"month", 
-			"day", 
-			"hour", 
-			"flow.vector", 
-			"wind.speed", 
-			"temperature", 
-			"stability.class", 
-			"rural.mixing.height", 
+			"year",
+			"month",
+			"day",
+			"hour",
+			"flow.vector",
+			"wind.speed",
+			"temperature",
+			"stability.class",
+			"rural.mixing.height",
 			"urban.mixing.height")
 	)
-	
+
 	# Label the rows of the resulting data frame.
-	row.names(result$records) <- with(result$records, 
+	row.names(result$records) <- with(result$records,
 		sprintf('%02d-%02d-%02d %02d:00', year, month, day, hour-1))
-		
+
 	# Rotate the flow vector by 180 degrees to obtain the wind bearing.
 	rotate <- function(angle, by) ((angle + by) + 360) %% 360
 	result$records$wind.bearing <- with(result$records, rotate(flow.vector, 180.0))
 	result$records$flow.vector <- NULL
 		return(result)
-	
+
 }

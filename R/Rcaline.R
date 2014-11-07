9c08e43188b3e06334301245d650420a75192d72
David Holstius <david.holstius@gmail.com>
2014-11-07 15:22:27 -0800

    remove .onLoad, .onAttach, and .onUnload

--- R/Rcaline.R
+++ R/Rcaline.R
@@ -1,9 +1,3 @@
-.onLoad <- function(libname=NULL, pkgname="Rcaline") {
-	try(library.dynam('Rcaline', pkgname, libname))
-}
-.onAttach <- function(libname, pkgname) {}
-.onUnload <- function(libpath) library.dynam.unload('Rcaline', libpath)
-
 #' Rcaline provides an interface to Fortran implementations of the CALINE model family. These steady-state, Gaussian dispersion models are used to predict aerosol concentrations downwind from mobile emission sources such as highway traffic.
 #'
 #' @name Rcaline
@@ -12,7 +6,7 @@
 #' @title Rcaline
 #' @author David Holstius \email{david.holstius@@berkeley.edu}
 #'
-#' @references 
+#' @references
 #' Benson, P. (1979) CALINE-3: A versatile dispersion model for predicting air pollution levels near highways and urban streets. Federal Highway Authority report FHWA/CA/TL-79/23, California DOT, Sacramento (1979).
 #'
 #' @keywords package environmental modeling disperson CALINE CALINE3 traffic

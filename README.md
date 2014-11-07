33f6d89f93b406895ee4c2c2515e34ffe6e32018
David Holstius <david.holstius@gmail.com>
2014-11-07 15:51:26 -0800

    update vignettes and README

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ R interface to CALINE3 steady-state Gaussian plume model
 Installation:
 
     if (!require(devtools)) install.packages("devtools")
-    devtools::install_github("BAAQMD/Rcaline")
+    devtools::install_github("BAAQMD/Rcaline", build_vignettes = TRUE)
--- inst/doc/Rcaline.R
+++ inst/doc/Rcaline.R
@@ -1,19 +1,4 @@
-### R code from vignette source 'Rcaline.Rnw'
-
-###################################################
-### code chunk number 1: Rcaline.Rnw:20-22
-###################################################
-options(width=60, continue=" ")
-options(SweaveHooks=list(fig=function() par(mar=c(5.1, 4.1, 1.1, 2.1))))
-
-
-###################################################
-### code chunk number 2: Rcaline.Rnw:30-35
-###################################################
-options(
-	width=60, 
-	continue=" ", 
-	SweaveHooks=list(fig=function() par(mar=c(5.1, 4.1, 1.1, 2.1)))
-	)
-
+## ----echo = FALSE-----------------------------------------
+options(width = 60, continue = " ")
+#options(SweaveHooks=list(fig=function() par(mar=c(5.1, 4.1, 1.1, 2.1))))
 

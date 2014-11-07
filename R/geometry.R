58aaf521b7e55b88f8ef18428ed68b9e7fe7ebc7
David Holstius <david.holstius@gmail.com>
2014-11-07 15:22:06 -0800

    move ahead with extract_segments()

--- R/geometry.R
+++ R/geometry.R
@@ -1,9 +1,9 @@
 as.SpatialLines.SpatialPolygons <- function(spobj) {
-    stopifnot(class(spobj)=="SpatialPolygons")
-    as.Lines <- function(polygons) Lines(lapply(polygons@Polygons, Line), ID=polygons@ID)
-    result <- SpatialLines(lapply(spobj@polygons, as.Lines))
-    proj4string(result) <- proj4string(spobj)
-    return(result)
+  stopifnot(class(spobj) == "SpatialPolygons")
+  as.Lines <- function(polygons) Lines(lapply(polygons@Polygons, Line), ID=polygons@ID)
+  result <- SpatialLines(lapply(spobj@polygons, as.Lines))
+  proj4string(result) <- proj4string(spobj)
+  return(result)
 }
 
 require(sp)
@@ -11,28 +11,39 @@ as.SpatialLines <- function(spobj) stop(paste("Sorry, I don't know what to do wi
 setGeneric("as.SpatialLines")
 setMethod("as.SpatialLines", "SpatialPolygons", as.SpatialLines.SpatialPolygons)
 
-# Generic function for splitting polylines into segments
-setGeneric("decimate", function(x, ...) standardGeneric("decimate"))
+#' Extract segments from a polyline or polylines
+#'
+#' @param x Lines or SpatialLines object
+#' @return matrix of coordinates
+#' @export
+extract_segments <- function(x, ...) standardGeneric("extract_segments")
+setGeneric("extract_segments", extract_segments)
 
-decimate.default <- function(x, suffixes=c(".start",".end")) {
-	coords <- coordinates(x)
-	result <- cbind(coords[-nrow(coords),,drop=FALSE], coords[-1,,drop=FALSE])
-	if(!is.null(colnames(coords)))
-		colnames(result) <- paste(colnames(coords), rep(suffixes, each=ncol(coords)), sep="")
-	return(result)
+#' @rdname extract_segments
+#' @export
+extract_segments.default <- function(x, suffixes=c(".start", ".end")) {
+  coords <- coordinates(x)
+  result <- cbind(coords[-nrow(coords),,drop=FALSE], coords[-1,,drop=FALSE])
+  if(!is.null(colnames(coords)))
+    colnames(result) <- paste(colnames(coords), rep(suffixes, each=ncol(coords)), sep="")
+  return(result)
 }
 
-decimate.Lines <- function(x, ...) {
-	nested <- rapply(x@Lines, decimate.default, ..., how="list")
-	flattened <- do.call(rbind, nested)
-	rownames(flattened) <- rep(x@ID, nrow(flattened))
-	return(flattened)
+#' @rdname extract_segments
+#' @export
+extract_segments.Lines <- function(x, ...) {
+  nested <- rapply(x@Lines, extract_segments.default, ..., how="list")
+  flattened <- do.call(rbind, nested)
+  rownames(flattened) <- rep(x@ID, nrow(flattened))
+  return(flattened)
 }
 
-decimate.SpatialLines <- function(x, ...) {
-	nested <- lapply(x@lines, decimate.Lines)
-	do.call(rbind, nested)
+#' @rdname extract_segments
+#' @export
+extract_segments.SpatialLines <- function(x, ...) {
+  nested <- lapply(x@lines, extract_segments.Lines)
+  do.call(rbind, nested)
 }
 
-setMethod("decimate", "Lines", decimate.Lines)
-setMethod("decimate", "SpatialLines", decimate.SpatialLines)
+setMethod("extract_segments", "Lines", extract_segments.Lines)
+setMethod("extract_segments", "SpatialLines", extract_segments.SpatialLines)
